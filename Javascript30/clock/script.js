196601a7741240c62a53303a3d11cdd33de995b2
emu <bsse0927@iit.du.ac.bd>
2020-03-16 16:41:55 +0600

    updated

--- Javascript30/clock/script.js
+++ Javascript30/clock/script.js
@@ -4,7 +4,23 @@ const hourHand = document.querySelector('.hour-hand');
 
 function setDate() {
 
-    console.log('hi');
+    const date = new Date();
+    const seconds= date.getSeconds();
+    const secondDegree = ((seconds / 60) * 360) +90;
+
+    secondHand.style.transform = `rotate(${secondDegree}deg)`;
+
+    const minutes = date.getMinutes();
+    const minDegree = ((minutes / 60) * 360) + 90;
+
+    minsHand.style.transform = `rotate(${minDegree}deg)`;
+
+    const hours = date.getHours();
+    const hourDegree = ((hours / 60) * 360) + 90;
+
+    hourHand.style.transform = `rotate(${hourDegree}deg)`;
+
+    console.log(seconds);
 }
 
 setInterval(setDate, 1000);
