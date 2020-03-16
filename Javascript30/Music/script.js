e27bf51875a48038de67025c445db3837b753758
emu <bsse0927@iit.du.ac.bd>
2020-03-16 13:12:55 +0600

    music done

--- Javascript30/Music/script.js
+++ Javascript30/Music/script.js
@@ -1,17 +1,22 @@
-window.addEventListener('keydown', function(e) {
+function playSound(e) {
     const audio = this.document.querySelector(`audio[data-key="${e.keyCode}"]`);
     const key = this.document.querySelector(`div[data-key="${e.keyCode}"]`);
     //console.log(audio);
-    if(!audio) return;
-     audio.currentTime = 0;
+    if (!audio) return;
+    audio.currentTime = 0;
     audio.play();
-
     key.classList.add('playing');
+}
+
+
+
+window.addEventListener('keydown', playSound);    
 
     function removeTransition(e)    {
-        console.log(e);
+        //console.log(e);
+        if(e.propertyName!='transform') return;
+        this.classList.remove('playing');
     }
     // keys.forEach(key => key.addEventListener('transitionend', removeTransition));
     const keys = Array.from(document.querySelectorAll('.key'));
     keys.forEach(key => key.addEventListener('transitionend', removeTransition));
-});
